4d143aa86e1c8a66963c402cd64ec15b4d04271c
zakiakhtar <31188896+zakiakhtar@users.noreply.github.com>
2017-09-10 01:29:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-})
+});
 
 app.get('/article-one' , function  (req , res) { 
-       res.send('article one requested and will be served here');
+         res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/article-two' , function  (req , res) { 
