8c008b5c68006e951d77a93a5281fd26620baa1c
zakiakhtar <31188896+zakiakhtar@users.noreply.github.com>
2017-09-10 00:50:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,18 +9,6 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });r
 
-app.get('/article-one' , function  (req , res) { 
-       res.send('article one requested and will be served here');
-});
-
-app.get('/article-two' , function  (req , res) { 
-       res.send('article two requested and will be served here');
-});
-
-app.get('/article-three' , function  (req , res) { 
-       res.send('article three requested and will be served here');
-});
-
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
